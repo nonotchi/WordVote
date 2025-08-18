12b82675bcee51e20ac77309be93f8b6b21f2772
nonotchi <197896352+nonotchi@users.noreply.github.com>
2025-08-19 08:06:14 +0900

    投票機能を実装しました。

--- vote/vote.js
+++ vote/vote.js
@@ -19,3 +19,123 @@ modalBack.addEventListener('focus', () => {
     modalBack.style.display = 'none';
     modal.style.display = 'none';
 });
+
+let words = [
+    {
+        id: "test1",
+        str: "これはテストです",
+        votes: 0,
+        isVoted: false,
+        rank: 0,
+    },
+    {
+        id: "test2",
+        str: "これはテストだよ。",
+        votes: 2,
+        isVoted: true,
+        rank: 0,
+    },
+    {
+        id: "test3",
+        str: "てすとだよーん",
+        votes: 0,
+        isVoted: false,
+        rank: 0,
+    },
+    {
+        id: "test4",
+        str: "This is a test.",
+        votes: 4,
+        isVoted: true,
+        rank: 0,
+    },
+];
+
+let idCount = 5;
+
+update = () => {
+    const board = document.getElementById('board');
+    board.innerHTML = '';
+
+    // TODO: IPアドレスで複数投票を制限
+
+    // ランキング更新
+    let sorted = [...words].sort((a, b) => b.votes - a.votes);
+    
+    let currentRank = 1;
+    let lastVotes = null;
+    let rankMap = new Map();
+
+    sorted.forEach((item, index) => {
+        if (item.votes !== lastVotes) {
+            currentRank = index + 1; // 新しい順位
+            lastVotes = item.votes;
+        }
+        rankMap.set(item.id, currentRank);
+    });
+
+    // rankを反映
+    words = words.map(item => ({
+        ...item,
+        rank: rankMap.get(item.id)
+    }));
+
+    // 表示
+
+    for (let i = 0; i < words.length; i++) {
+        const elem = document.createElement('div');
+        elem.className = 'word';
+        elem.setAttribute('count', words[i].rank == 0 ? '' : String(words[i].rank));
+
+        const wordStr = document.createElement('div');
+        wordStr.className = 'str';
+        wordStr.innerText = words[i].str;
+
+        const votesCount = document.createElement('div');
+        votesCount.className = 'count';
+        votesCount.innerText = `${words[i].votes}票`;
+
+        // TODO: 投票ボタン
+
+        const voteBtn = document.createElement('button');
+        voteBtn.className = 'vote-button';
+        voteBtn.innerText = `投票する！`;
+
+        // TODO: 複数回投票できないようにするには、ここのコメントを外す
+        /*if (words[i].isVoted) {
+            voteBtn.disabled = true;
+        }*/
+
+        voteBtn.addEventListener('click', () => {
+            words[i].votes += 1;
+            words[i].isVoted = true;
+            update();
+        });
+        
+        elem.appendChild(wordStr);
+        elem.appendChild(votesCount);
+        elem.appendChild(voteBtn);
+
+        elem.style.animationDelay = `${String(i * 0.02)}s`
+
+        board.appendChild(elem);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', update);
+
+document.getElementById('add').addEventListener('click', () => {
+    words.push({
+        id: `test${idCount}`,
+        str: document.getElementById('word').value,
+        votes: 0,
+        isVoted: false,
+        rank: 0,
+    });
+
+    document.getElementById('word').value = '';
+    update();
+
+    modalBack.style.display = 'none';
+    modal.style.display = 'none';
+});
